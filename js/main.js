04878ba320d46e7bd1e77ea07289992272d9577f
Sebastian-Estobar <sebastianestobar@gmail.com>
2026-06-09 12:57:14 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
+// ==========================
+// INIT SIGMA
+// ==========================
 function initSigma(config) {
     var data = config.data;
 
-    var a = sigma.init(document.getElementById("sigma-canvas"))
+    // REPARACIÓN DEFINITIVA: Forzar dimensiones físicas reales en el contenedor antes de iniciar Sigma
+    var canvasDiv = document.getElementById("sigma-canvas");
+    if (canvasDiv) {
+        var parentWidth = canvasDiv.parentElement ? canvasDiv.parentElement.clientWidth : window.innerWidth;
+        var parentHeight = canvasDiv.parentElement ? canvasDiv.parentElement.clientHeight : window.innerHeight;
+        
+        // Si el contenedor mide 0 o no ha cargado, asignamos el viewport completo de respaldo
+        if (!parentWidth || parentWidth === 0) parentWidth = window.innerWidth;
+        if (!parentHeight || parentHeight === 0) parentHeight = window.innerHeight;
+        
+        canvasDiv.style.width = parentWidth + "px";
+        canvasDiv.style.height = parentHeight + "px";
+    }
+
+    var a = sigma.init(canvasDiv)
         .drawingProperties(config.sigma.drawingProperties)
         .graphProperties(config.sigma.graphProperties)
         .mouseProperties(config.sigma.mouseProperties);
@@ -35,22 +52,30 @@ function initSigma(config) {
             nodeActive(nodeId);
         });
 
-        // Forzar el dibujo con un desfase mínimo para dar tiempo al render en producción
+        // Aseguramos que Sigma recalcule su layout interno con las medidas fijadas
+        if (typeof a.resize === "function") {
+            a.resize();
+        }
+
+        // Forzar centrado automático de la cámara para que el grafo aparezca en pantalla
+        if (a._core && a._core.camera) {
+            a.position(0, 0, 1);
+        }
+
         a.draw();
         
+        // Un segundo pase de renderizado preventivo por si el DOM tardó en estabilizarse
         setTimeout(function() {
-            if (typeof sigInst.resize === "function") {
-                sigInst.resize();
-            }
+            if (typeof sigInst.resize === "function") sigInst.resize();
             sigInst.draw();
-        }, 100);
+        }, 150);
         
         configSigmaElements(config);
         setupZoomButtons();
     }
 
-    if (data.indexOf("gexf")>0 || data.indexOf("xml")>0)
-        a.parseGexf(data,dataReady);
+    if (data.indexOf("gexf") > 0 || data.indexOf("xml") > 0)
+        a.parseGexf(data, dataReady);
     else
-        a.parseJson(data,dataReady);
+        a.parseJson(data, dataReady);
 }
